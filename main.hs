f3b3af1c0fb06f75aec2570d2d45a7eef6c5f8ea
JesusOliveto <ignaolariaga@gmail.com>
2022-08-18 17:42:58 -0300

    comments

--- main.hs
+++ main.hs
@@ -1,3 +1,4 @@
+--clase 1
 cuadrado::Integer->Integer
 cuadrado x = x * x
 
@@ -9,6 +10,8 @@ parTexto x = if (mod x 2 == 0) then "par" else "impar"
 
 menor::(Integer,Integer)->Bool
 menor (x,y) = if (x<=y) then True else False
+
+--EJERCITACION
 
 absoluto::Integer->Integer
 absoluto x = if (x<0) then (-x) else x
@@ -28,11 +31,13 @@ doble x=x*2
 filterPositivos::[Integer]->[Integer]
 filterPositivos xs = filter (>0) xs
 
---filterPostivos recursivo
 filterPositivosRec::[Integer]->[Integer]
 filterPositivosRec [] = []
 filterPositivosRec (x:xs) = if (x>0) then x:filterPositivosRec xs else filterPositivosRec xs
 
+--FIN EJERCITACION
+
+--clase 2
 add::Integer->Integer->Integer
 add x y = x+y
 main = do 
