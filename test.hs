ba7bc67436f884de06819c8893dc6693bb539d5e
JesusOliveto <ignaolariaga@gmail.com>
2022-08-18 17:17:45 -0300

    arreglao

--- test.hs
+++ test.hs
@@ -10,11 +10,11 @@ parTexto x = if (mod x 2 == 0) then "par" else "impar"
 menor::(Integer,Integer)->Bool
 menor (x,y) = if (x<=y) then True else False
 
-abs::Integer->Integer
-abs x = if (x<0) then (-x) else x
+absoluto::Integer->Integer
+absoluto x = if (x<0) then (-x) else x
 
-signnum::Integer->Integer
-signnum x = if (x>0) then 1 else if (x<0) then -1 else 0
+signumRedef::Integer->Integer
+signumRedef x = if (x>0) then 1 else if (x<0) then -1 else 0
 
 anterior::Integer->Integer
 anterior x= if (x>0) then x-1 else error "No hay anterior"
@@ -27,3 +27,8 @@ doble x=x*2
 
 filterPositivos::[Integer]->[Integer]
 filterPositivos xs = filter (>0) xs
+
+--filterPostivos recursivo
+filterPositivosRec::[Integer]->[Integer]
+filterPositivosRec [] = []
+filterPositivosRec (x:xs) = if (x>0) then x:filterPositivosRec xs else filterPositivosRec xs
