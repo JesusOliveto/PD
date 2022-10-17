b136a676e3f7c3994eefcc737941983bc6fa6d89
JesusOliveto <ignaolariaga@gmail.com>
2022-10-17 17:23:02 -0300

    auxiliares, separarlos por funciones

--- clickers/clicker.py
+++ clickers/clicker.py
@@ -9,14 +9,29 @@ pygame.init()
 #variables
 
 clock = pygame.time.Clock()
-deltaR = 0.0
-r=0.0
-A=0.0
-a=1.0
-b=1.0
-c=1.0
-d=1.0
-k=1.0
+
+data = {
+    "deltaR" : 0.0,
+    'r': 0.0,
+    'A': 0.0,
+    'a': 1.0,
+    'b': 1.0,
+    'c': 1.0,
+    'd': 1.0,
+    'k': 1.0,
+    'costa': 100,
+    'costb': 500,
+    'costc': 1000,
+    'costd': 10000,
+    'costk': 10,
+}
+
+global auxa
+global auxb
+global auxc
+global auxd
+global auxk
+
 black=(0,0,0)
 white=(255,255,255)
 red=(255,0,0)
@@ -42,99 +57,130 @@ def rectangle(display, color, x, y, w, h):
     pygame.draw.rect(display, color, (x, y, w, h))
     
 def rincrement():
-    global r
-    global a 
-    global b 
-    global c
-    global d
-    global k
-    global A 
-    global deltaR
-    A= (a*b*c*d)
-    r= (r + A + k)
-    A= round(A,5)
+    data["A"]=(data["a"]*data["b"]*data["c"]*data["d"])
+    data["r"]= (data["r"] + data["A"] + data["k"])
+    data["A"]= round(data["A"],5)
     #print("r=",r)
     clock.tick(30)
-    deltaR= (A+k) *30
+    data["deltaR"]= (data["A"]+data["k"]) *30
 
-
+def getAux():
+    global auxa
+    global auxb
+    global auxc
+    global auxd
+    global auxk
+    
+    auxa=0
+    auxb=0
+    auxc=0
+    auxd=0
+    auxk=0
+    
+    r= data["r"]
+    costa= data["costa"]
+    while r >= costa:
+        auxa= auxa + 1
+        r= r - costa
+        costa= costa * 1.05
+    
+    r= data["r"]
+    costb= data["costb"]
+    while r >= costb:
+        auxa= auxa + 1
+        r= r - costb
+        costb= costb * 1.05
+    
+    r= data["r"]
+    costc= data["costc"]
+    while r >= costc:
+        auxa= auxa + 1
+        r= r - costc
+        costc= costc * 1.05
+    
+    r= data["r"]
+    costd= data["costd"]
+    while r >= costd:
+        auxa= auxa + 1
+        r= r - costd
+        costd= costd * 1.05
+    
+    r= data["r"]
+    costk= data["costk"]
+    while r >= costk:
+        auxa= auxa + 1
+        r= r - costk
+        costk= costk * 1.05
+    
     
 def main_loop():
-    global r 
-    global a 
-    global b 
-    global c
-    global d
-    global k
-    global A
-    costa=100
-    costb=500
-    costc=1000
-    costd=10000
-    costk=10
+    
     
     game_running = True
     while game_running:
         if game_running: 
             rincrement()
+            getAux()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mopos = pygame.mouse.get_pos()
                 if mopos[0] > 300 and mopos[0] < 500 and mopos[1] > 100 and mopos[1] < 200:
-                    if r >= costa:
+                    if data["r"] >= data["costa"]:
                         #test buymax
-                        aux= r/costa
-                        aux=int(aux)
-                        r= r - costa * aux
-                        costa= costa * 1.05 * aux
-                        a= a + 0.1 * aux
-                        costa= round(costa,2)
-                        a= round(a,2)
-                        print("total a bought:",aux)
+                        global auxa
+                        for i in range(auxa):
+                            if data["r"] >= data["costa"]:
+                                data["a"] = data["a"] + 0.1
+                                data["r"] = data["r"] - data["costa"]
+                                data["costa"] = data["costa"] * 1.05
+                                data["costa"] = round(data["costa"], 2)
+                                data["a"] = round(data["a"], 2)
+                        print("total a bought:",auxa)
                         
                 if mopos[0] > 300 and mopos[0] < 500 and mopos[1] > 300 and mopos[1] < 400:
-                    if r >= costb:
-                        aux= r/costb
-                        aux=int(aux)
-                        r= r - costb * aux
-                        costb= costb * 1.1 * aux
-                        b= b + 0.1 * aux
-                        costb= round(costb,2)
-                        b= round(b,2)
-                        print("total b bought:",aux)
+                    if data["r"] >= data["costb"]:
+                        global auxb
+                        for i in range(auxb):
+                            data["r"]= data["r"] - data["costb"]
+                            data["costb"]= data["costb"] * 1.05
+                            data["b"]= data["b"] + 0.1
+                            data["costb"]= round(data["costb"],2)
+                            data["b"]= round(data["b"],2)
+                        print("total b bought:",auxb)
                 if mopos[0] > 300 and mopos[0] < 500 and mopos[1] > 500 and mopos[1] < 600:
-                    if r >= costc:
-                        aux= r/costc
-                        aux=int(aux)
-                        r= r - costc * aux
-                        costc= costc * 1.11 * aux
-                        c= c + 0.1 * aux
-                        costc= round(costc,2)
-                        c= round(c,2)
-                        print("total c bought:",aux)
+                    if data["r"] >= data["costc"]:
+                        global auxc
+                        for i in range(auxc):
+                            data["r"]= data["r"] - data["costc"]
+                            data["costc"]= data["costc"] * 1.05
+                            data["c"]= data["c"] + 0.1
+                            data["costc"]= round(data["costc"],2)
+                            data["c"]= round(data["c"],2)
+                        print("total c bought:",auxc)
                 if mopos[0] > 600 and mopos[0] < 800 and mopos[1] > 100 and mopos[1] < 200:
-                    if r >= costd:
-                        aux= r/costd
-                        aux=int(aux)
-                        r= r - costd * aux
-                        costd= costd * 1.12* aux
-                        d= d + 0.1 * aux
-                        costd= round(costd,2)
-                        d= round(d,2)
-                        print("total d bought:",aux)
+                    if data["r"] >= data["costd"]:
+                        global auxd
+                        for i in range(auxd):
+                            data["r"]= data["r"] - data["costd"]
+                            data["costd"]= data["costd"] * 1.05
+                            data["d"]= data["d"] + 0.1
+                            data["costd"]= round(data["costd"],2)
+                            data["d"]= round(data["d"],2)
+                        print("total d bought:",auxd)
                     
                 if mopos[0] > 600 and mopos[0] < 800 and mopos[1] > 300 and mopos[1] < 400:
-                    if r >= costk:
-                        aux= r/costk
-                        aux=int(aux)
-                        r= r - costk * aux
-                        costk= costk * 1.1 * aux
-                        k= k + 1 * aux
-                        costk= round(costk,2)
-                        k= round(k,2)
-                        print("total k bought:",aux)
+                    if data["r"] >= data["costk"]:
+                        global auxk
+                        for i in range(auxk):
+                            if data["r"] >= data["costk"]:
+                                data["r"]= data["r"] - data["costk"]
+                                data["costk"]= data["costk"] * 1.05
+                                data["k"]= data["k"] + 1
+                                data["costk"]= round(data["costk"],2)
+                                data["k"]= round(data["k"],2)
+                        print("total k bought:",auxk)
         
         gameDisplay.fill(black)
         
@@ -144,36 +190,36 @@ def main_loop():
             pygame.draw.line(gameDisplay, (255, 255, 255), (i, 0), (i, 900))
             
         #info
-        DrawText("r = " + str(f'{r:.2f}') , white, black, 100, 50, 20) 
-        DrawText("deltaR = " + str(f'{deltaR:.2f}') , white, black, 100, 75, 20)
+        DrawText("r = " + str(f'{data["r"]:.2f}') , white, black, 100, 50, 20) 
+        DrawText("deltaR = " + str(f'{data["deltaR"]:.2f}') , white, black, 100, 75, 20)
         rectangle(gameDisplay, grey, 0, 100, 200, 400)
         DrawText("r = A + k"  , white, black, 100, 110, 20)      
         DrawText("A= a*b*c*d"  , white, black, 100, 130, 20)
         
         #mejoras
-        DrawText("a = " + str(a)  , white, black, 100, 150, 20)  
-        DrawText("b = " + str(b)  , white, black, 100, 170, 20)
-        DrawText("c = " + str(c)  , white, black, 100, 190, 20)
-        DrawText("d = " + str(d)  , white, black, 100, 210, 20)
-        DrawText("k = " + str(k)  , white, black, 100, 230, 20)
-        DrawText("A = " + str(A)  , white, black, 100, 250, 20)
+        DrawText("a = " + str(data["a"])  , white, black, 100, 150, 20)  
+        DrawText("b = " + str(data["b"])  , white, black, 100, 170, 20)
+        DrawText("c = " + str(data["c"])  , white, black, 100, 190, 20)
+        DrawText("d = " + str(data["d"])  , white, black, 100, 210, 20)
+        DrawText("k = " + str(data["k"])  , white, black, 100, 230, 20)
+        DrawText("A = " + str(data["A"])  , white, black, 100, 250, 20)
         
         #botones
         rectangle(gameDisplay, grey, 300, 100, 200, 100)
-        DrawText("a+0.1 = " + str(costa)  , white, black, 400, 150, 20)
-        DrawText("+" + str(int(r/costa))  , white, black, 400, 170, 20) 
+        DrawText("a+0.1 = " + str(data["costa"])  , white, black, 400, 150, 20)
+        DrawText("+" + str(int(auxa))  , white, black, 400, 170, 20) 
         rectangle(gameDisplay, grey, 300, 300, 200, 100)
-        DrawText("b+0.1 = " + str(costb)  , white, black, 400, 350, 20)
-        DrawText("+" + str(int(r/costb))  , white, black, 400, 370, 20)
+        DrawText("b+0.1 = " + str(data["costb"])  , white, black, 400, 350, 20)
+        DrawText("+" + str(int(auxb))  , white, black, 400, 370, 20)
         rectangle(gameDisplay, grey, 300, 500, 200, 100)
-        DrawText("c+0.1 = " + str(costc)  , white, black, 400, 550, 20)
-        DrawText("+" + str(int(r/costc))  , white, black, 400, 570, 20)
+        DrawText("c+0.1 = " + str(data["costc"])  , white, black, 400, 550, 20)
+        DrawText("+" + str(int(auxc))  , white, black, 400, 570, 20)
         rectangle(gameDisplay, grey, 600, 100, 200, 100)
-        DrawText("d+0.1 = " + str(costd)  , white, black, 700, 150, 20)
-        DrawText("+" + str(int(r/costd))  , white, black, 700, 170, 20)
+        DrawText("d+0.1 = " + str(data["costd"])  , white, black, 700, 150, 20)
+        DrawText("+" + str(int(auxd))  , white, black, 700, 170, 20)
         rectangle(gameDisplay, grey, 600, 300, 200, 100)
-        DrawText("k+1 = " + str(costk)  , white, black, 700, 350, 20)
-        DrawText("+" + str(int(r/costk))  , white, black, 700, 370, 20)
+        DrawText("k+1 = " + str(data["costk"])  , white, black, 700, 350, 20)
+        DrawText("+" + str(int(auxk))  , white, black, 700, 370, 20)
         pygame.display.update()
         clock.tick(60)
 
