0f2516b3642591dcd94191b893b7845627280a7e
JesusOliveto <ignaolariaga@gmail.com>
2022-10-20 19:48:16 -0300

    funca el guardado, variables globales, soy D10S

--- clickers/clicker.py
+++ clickers/clicker.py
@@ -1,5 +1,6 @@
 #imports
 from funciones import *
+import data_
 
 pygame.init()
 
@@ -7,23 +8,14 @@ pygame.init()
 
 
 
-
-try:
-    with open ("savegame.txt") as save_file:
-        data=json.load(save_file)
-except:
-    print("no file created yet")
-
-try:
-    with open ("var.txt") as var_file:
-        Aux=json.load(var_file)
-
-
 #MAIN LOOP
 def main_loop():
     game_running = True
+    loadGame()
+    time.sleep(1)
+    print("data_.data LOADED MAIN: ", data_.data)
     while game_running:
-        if game_running: 
+        if game_running:
             rincrement()
             getAuxa()
             getAuxb()
@@ -38,56 +30,56 @@ def main_loop():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mopos = pygame.mouse.get_pos()
                 if mopos[0] > 300 and mopos[0] < 500 and mopos[1] > 100 and mopos[1] < 200:
-                    if data["r"] >= data["costa"]:
-                        for i in range(Aux["auxa"]):
-                            if data["r"] >= data["costa"]:
-                                data["a"] = data["a"] + 0.1
-                                data["r"] = data["r"] - data["costa"]
-                                data["costa"] = data["costa"] * 1.05
-                                data["costa"] = round(data["costa"], 2)
-                                data["a"] = round(data["a"], 2)
-                        print("total a bought:",Aux["auxa"])
+                    if data_.data["r"] >= data_.data["costa"]:
+                        for i in range(data_.Aux["auxa"]):
+                            if data_.data["r"] >= data_.data["costa"]:
+                                data_.data["a"] = data_.data["a"] + 0.1
+                                data_.data["r"] = data_.data["r"] - data_.data["costa"]
+                                data_.data["costa"] = data_.data["costa"] * 1.05
+                                data_.data["costa"] = round(data_.data["costa"], 2)
+                                data_.data["a"] = round(data_.data["a"], 2)
+                        print("total a bought:",data_.Aux["auxa"])
                         
                 if mopos[0] > 300 and mopos[0] < 500 and mopos[1] > 300 and mopos[1] < 400:
-                    if data["r"] >= data["costb"]:
-                        for i in range(Aux["auxb"]):
-                            data["r"]= data["r"] - data["costb"]
-                            data["costb"]= data["costb"] * 1.05
-                            data["b"]= data["b"] + 0.1
-                            data["costb"]= round(data["costb"],2)
-                            data["b"]= round(data["b"],2)
-                        print("total b bought:",Aux["auxb"])
+                    if data_.data["r"] >= data_.data["costb"]:
+                        for i in range(data_.Aux["auxb"]):
+                            data_.data["r"]= data_.data["r"] - data_.data["costb"]
+                            data_.data["costb"]= data_.data["costb"] * 1.05
+                            data_.data["b"]= data_.data["b"] + 0.1
+                            data_.data["costb"]= round(data_.data["costb"],2)
+                            data_.data["b"]= round(data_.data["b"],2)
+                        print("total b bought:",data_.Aux["auxb"])
                 if mopos[0] > 300 and mopos[0] < 500 and mopos[1] > 500 and mopos[1] < 600:
-                    if data["r"] >= data["costc"]:
-                        for i in range(Aux["auxc"]):
-                            data["r"]= data["r"] - data["costc"]
-                            data["costc"]= data["costc"] * 1.05
-                            data["c"]= data["c"] + 0.1
-                            data["costc"]= round(data["costc"],2)
-                            data["c"]= round(data["c"],2)
-                        print("total c bought:",Aux["auxc"])
+                    if data_.data["r"] >= data_.data["costc"]:
+                        for i in range(data_.Aux["auxc"]):
+                            data_.data["r"]= data_.data["r"] - data_.data["costc"]
+                            data_.data["costc"]= data_.data["costc"] * 1.05
+                            data_.data["c"]= data_.data["c"] + 0.1
+                            data_.data["costc"]= round(data_.data["costc"],2)
+                            data_.data["c"]= round(data_.data["c"],2)
+                        print("total c bought:",data_.Aux["auxc"])
                 if mopos[0] > 600 and mopos[0] < 800 and mopos[1] > 100 and mopos[1] < 200:
-                    if data["r"] >= data["costd"]:
-                        for i in range(Aux["auxd"]):
-                            data["r"]= data["r"] - data["costd"]
-                            data["costd"]= data["costd"] * 1.05
-                            data["d"]= data["d"] + 0.1
-                            data["costd"]= round(data["costd"],2)
-                            data["d"]= round(data["d"],2)
-                        print("total d bought:",Aux["auxd"])
+                    if data_.data["r"] >= data_.data["costd"]:
+                        for i in range(data_.Aux["auxd"]):
+                            data_.data["r"]= data_.data["r"] - data_.data["costd"]
+                            data_.data["costd"]= data_.data["costd"] * 1.05
+                            data_.data["d"]= data_.data["d"] + 0.1
+                            data_.data["costd"]= round(data_.data["costd"],2)
+                            data_.data["d"]= round(data_.data["d"],2)
+                        print("total d bought:",data_.Aux["auxd"])
                     
                 if mopos[0] > 600 and mopos[0] < 800 and mopos[1] > 300 and mopos[1] < 400:
-                    if data["r"] >= data["costk"]:
-                        for i in range(Aux["auxk"]):
-                            if data["r"] >= data["costk"]:
-                                data["r"]= data["r"] - data["costk"]
-                                data["costk"]= data["costk"] * 1.05
-                                data["k"]= data["k"] + 1
-                                data["costk"]= round(data["costk"],2)
-                                data["k"]= round(data["k"],2)
-                        print("total k bought:",Aux["auxk"])
+                    if data_.data["r"] >= data_.data["costk"]:
+                        for i in range(data_.Aux["auxk"]):
+                            if data_.data["r"] >= data_.data["costk"]:
+                                data_.data["r"]= data_.data["r"] - data_.data["costk"]
+                                data_.data["costk"]= data_.data["costk"] * 1.05
+                                data_.data["k"]= data_.data["k"] + 1
+                                data_.data["costk"]= round(data_.data["costk"],2)
+                                data_.data["k"]= round(data_.data["k"],2)
+                        print("total k bought:",data_.Aux["auxk"])
                 if mopos[0] > 600 and mopos[0] < 800 and mopos[1] > 500 and mopos[1] < 600:
-                    if Aux["auxw"] >= 1:
+                    if data_.Aux["auxw"] >= 1:
                         prestige()
                         print("prestige")
                     else:
@@ -102,39 +94,39 @@ def main_loop():
             
         #info
         rectangle(gameDisplay, grey, 0, 0, 200, 400)
-        DrawText("r = " + str(f'{data["r"]:.2f}') , white, black, 100, 50, 20) 
-        DrawText("deltaR = " + str(f'{data["deltaR"]:.2f}') , white, black, 100, 75, 20)
+        DrawText("r = " + str(f'{data_.data["r"]:.2f}') , white, black, 100, 50, 20) 
+        DrawText("deltaR = " + str(f'{data_.data["deltaR"]:.2f}') , white, black, 100, 75, 20)
         DrawText("r = w*A + k"  , white, black, 100, 110, 20)      
         DrawText("A= a*b*c*d"  , white, black, 100, 130, 20)
         
         #mejoras
-        DrawText("a = " + str(data["a"])  , white, black, 100, 150, 20)  
-        DrawText("b = " + str(data["b"])  , white, black, 100, 170, 20)
-        DrawText("c = " + str(data["c"])  , white, black, 100, 190, 20)
-        DrawText("d = " + str(data["d"])  , white, black, 100, 210, 20)
-        DrawText("k = " + str(data["k"])  , white, black, 100, 230, 20)
-        DrawText("A = " + str(data["A"])  , white, black, 100, 250, 20)
-        DrawText("w = " + str(data["w"])  , white, black, 100, 270, 20)
+        DrawText("a = " + str(data_.data["a"])  , white, black, 100, 150, 20)  
+        DrawText("b = " + str(data_.data["b"])  , white, black, 100, 170, 20)
+        DrawText("c = " + str(data_.data["c"])  , white, black, 100, 190, 20)
+        DrawText("d = " + str(data_.data["d"])  , white, black, 100, 210, 20)
+        DrawText("k = " + str(data_.data["k"])  , white, black, 100, 230, 20)
+        DrawText("A = " + str(data_.data["A"])  , white, black, 100, 250, 20)
+        DrawText("w = " + str(data_.data["w"])  , white, black, 100, 270, 20)
         
         #botones
         rectangle(gameDisplay, grey, 300, 100, 200, 100)
-        DrawText("a+0.1 = " + str(data["costa"])  , white, black, 400, 150, 20)
-        DrawText("+" + str(int(Aux["auxa"]))  , white, black, 400, 170, 20) 
+        DrawText("a+0.1 = " + str(data_.data["costa"])  , white, black, 400, 150, 20)
+        DrawText("+" + str(int(data_.Aux["auxa"]))  , white, black, 400, 170, 20) 
         rectangle(gameDisplay, grey, 300, 300, 200, 100)
-        DrawText("b+0.1 = " + str(data["costb"])  , white, black, 400, 350, 20)
-        DrawText("+" + str(int(Aux["auxb"]))  , white, black, 400, 370, 20)
+        DrawText("b+0.1 = " + str(data_.data["costb"])  , white, black, 400, 350, 20)
+        DrawText("+" + str(int(data_.Aux["auxb"]))  , white, black, 400, 370, 20)
         rectangle(gameDisplay, grey, 300, 500, 200, 100)
-        DrawText("c+0.1 = " + str(data["costc"])  , white, black, 400, 550, 20)
-        DrawText("+" + str(int(Aux["auxc"]))  , white, black, 400, 570, 20)
+        DrawText("c+0.1 = " + str(data_.data["costc"])  , white, black, 400, 550, 20)
+        DrawText("+" + str(int(data_.Aux["auxc"]))  , white, black, 400, 570, 20)
         rectangle(gameDisplay, grey, 600, 100, 200, 100)
-        DrawText("d+0.1 = " + str(data["costd"])  , white, black, 700, 150, 20)
-        DrawText("+" + str(int(Aux["auxd"]))  , white, black, 700, 170, 20)
+        DrawText("d+0.1 = " + str(data_.data["costd"])  , white, black, 700, 150, 20)
+        DrawText("+" + str(int(data_.Aux["auxd"]))  , white, black, 700, 170, 20)
         rectangle(gameDisplay, grey, 600, 300, 200, 100)
-        DrawText("k+1 = " + str(data["costk"])  , white, black, 700, 350, 20)
-        DrawText("+" + str(int(Aux["auxk"]))  , white, black, 700, 370, 20)
+        DrawText("k+1 = " + str(data_.data["costk"])  , white, black, 700, 350, 20)
+        DrawText("+" + str(int(data_.Aux["auxk"]))  , white, black, 700, 370, 20)
         rectangle(gameDisplay, grey, 600, 500, 200, 100)
         DrawText("PRESTIGE"  , white, black, 700, 550, 20)
-        DrawText("W +" + str(int(Aux["auxw"]))  , white, black, 700, 570, 20)
+        DrawText("W +" + str(int(data_.Aux["auxw"]))  , white, black, 700, 570, 20)
         pygame.display.update()
         clock.tick(60)
 
--- clickers/funciones.py
+++ clickers/funciones.py
@@ -2,35 +2,12 @@ import pygame
 import time
 import math
 import json
+import data_
 
 
 clock = pygame.time.Clock()
 
-Aux = {
-    'auxa' : 0.0,
-    'auxb' : 0.0,
-    'auxc' : 0.0,
-    'auxd' : 0.0,
-    'auxk' : 0.0,
-    'auxw' : 0.0,
-}
 
-data = {
-    "deltaR" : 0.0,
-    'r': 0.0,
-    'A': 0.0,
-    'a': 1.0,
-    'b': 1.0,
-    'c': 1.0,
-    'd': 1.0,
-    'k': 1.0,
-    'w': 1.0,
-    'costa': 100,
-    'costb': 500,
-    'costc': 1000,
-    'costd': 10000,
-    'costk': 10,
-}
 
 black=(0,0,0)
 white=(255,255,255)
@@ -42,6 +19,22 @@ display_height=600
 #display
 gameDisplay=pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption("mierder clicker v0.1.2")
+
+
+def loadGame():
+    try:
+        with open ("savegame.txt") as save_file:
+            data_.data=json.load(save_file)
+    except:
+        print("no file created yet")
+
+    try:
+        with open ("var.txt") as var_file:
+            data_.Aux=json.load(var_file)
+    except:
+        print("no file created yet")
+    
+    print("data_.data LOADED: ", data_.data)
 
 
 def DrawText(text, Textcolor, Rectcolor, x, y, fsize):
@@ -56,83 +49,85 @@ def rectangle(display, color, x, y, w, h):
     pygame.draw.rect(display, color, (x, y, w, h))
     
 def rincrement():
-    data["A"]=(data["a"]*data["b"]*data["c"]*data["d"])
-    data["r"]= (data["r"] + data["A"] *data["w"]+ data["k"])
-    data["A"]= round(data["A"],5)
-    #print("r=",r)
+    data_.data["A"]=(data_.data["a"]*data_.data["b"]*data_.data["c"]*data_.data["d"])
+    data_.data["r"]= (data_.data["r"] + data_.data["A"] *data_.data["w"]+ data_.data["k"])
+    data_.data["A"]= round(data_.data["A"],5)
     clock.tick(30)
-    data["deltaR"]= (data["A"]* data["w"] +data["k"]) *30
+    data_.data["deltaR"]= (data_.data["A"]* data_.data["w"] +data_.data["k"]) *30
 
 
         
 def getAuxa():
-    Aux["auxa"]=0
-    r= data["r"]
-    costa= data["costa"]
+    data_.Aux["auxa"]=0
+    r= data_.data["r"]
+    costa= data_.data["costa"]
     while r >= costa:
-        Aux["auxa"]= Aux["auxa"] + 1
+        data_.Aux["auxa"]= data_.Aux["auxa"] + 1
         r= r - costa
         costa= costa * 1.05
 
 
 def getAuxb():
-    Aux["auxb"]=0
-    r= data["r"]
-    costb= data["costb"]
+    data_.Aux["auxb"]=0
+    r= data_.data["r"]
+    costb= data_.data["costb"]
     while r >= costb:
-        Aux["auxb"]= Aux["auxb"] + 1
+        data_.Aux["auxb"]= data_.Aux["auxb"] + 1
         r= r - costb
         costb= costb * 1.05
 
 def getAuxc():
-    Aux["auxc"]=0
-    r= data["r"]
-    costc= data["costc"]
+    data_.Aux["auxc"]=0
+    r= data_.data["r"]
+    costc= data_.data["costc"]
     while r >= costc:
-        Aux["auxc"]= Aux["auxc"] + 1
+        data_.Aux["auxc"]= data_.Aux["auxc"] + 1
         r= r - costc
         costc= costc * 1.05
 
 def getAuxd():
-    Aux["auxd"]=0
-    r= data["r"]
-    costd= data["costd"]
+    data_.Aux["auxd"]=0
+    r= data_.data["r"]
+    costd= data_.data["costd"]
     while r >= costd:
-        Aux["auxd"]= Aux["auxd"] + 1
+        data_.Aux["auxd"]= data_.Aux["auxd"] + 1
         r= r - costd
         costd= costd * 1.05
         
 def getAuxk():
-    Aux["auxk"]=0
-    r= data["r"]
-    costk= data["costk"]
+    data_.Aux["auxk"]=0
+    r= data_.data["r"]
+    costk= data_.data["costk"]
     while r >= costk:
-        Aux["auxk"]= Aux["auxk"] + 1
+        data_.Aux["auxk"]= data_.Aux["auxk"] + 1
         r= r - costk
         costk= costk * 1.05
 
 def getAuxw():
     
-    Aux["auxw"]= (data["a"]+data["b"]+data["c"]+data["d"])/10
-    Aux["auxw"]= int(Aux["auxw"]//1)
+    data_.Aux["auxw"]= (data_.data["a"]+data_.data["b"]+data_.data["c"]+data_.data["d"])/10
+    data_.Aux["auxw"]= int(data_.Aux["auxw"]//1)
 
 def saveGame():
-    with open("savegame.txt", "w") as save_file:
-        json.dump(data, save_file)
-    with open("var.txt", "w") as var_file:
-        json.dump(Aux, var_file)
-
+    try:
+        with open("savegame.txt", "w") as save_file:
+            json.dump(data_.data, save_file)
+        with open("var.txt", "w") as var_file:
+            json.dump(data_.Aux, var_file)
+        print("data_.data SAVED: ", data_.data)
+    except:
+        print("NO SE GUARDÓ CULIAOOOOOOOOOOOOOOOOOOOOOOOOOO")
 
 def prestige():
-    data["r"]=0
-    data["a"]=1
-    data["b"]=1
-    data["c"]=1
-    data["d"]=1
-    data["k"]=1
-    data["costa"]=100
-    data["costb"]=500
-    data["costc"]=1000
-    data["costd"]=10000
-    data["costk"]=10
-    data["w"]= data["w"]+ Aux["auxw"]
+    data_.data["r"]=0
+    data_.data["a"]=1
+    data_.data["b"]=1
+    data_.data["c"]=1
+    data_.data["d"]=1
+    data_.data["k"]=1
+    data_.data["costa"]=100
+    data_.data["costb"]=500
+    data_.data["costc"]=1000
+    data_.data["costd"]=10000
+    data_.data["costk"]=10
+    data_.data["w"]= data_.data["w"]+ data_.Aux["auxw"]
